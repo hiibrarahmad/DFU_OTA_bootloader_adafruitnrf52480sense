4ef41e42a1e9343f83a600bbf2895c24032a36e9
Ibrar Ahmad <hiibrarahmad@gmail.com>
2025-07-11 14:22:16 +0500

    WORKING EXAMPLE FOR NRF52840 DK

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,87 +1,112 @@
 #include <bluefruit.h>
 
-// Active LOW LED pins on nRF52840 DK
-#define LED1_PIN 13  // P0.13 (used by Bluefruit status LED by default)
-#define LED2_PIN 14  // P0.14
-#define LED3_PIN 15  // P0.15
-#define LED4_PIN 16  // P0.16
+// Active-LOW LED pins on nRF52840 DK
+#define LED1_PIN 13
+#define LED2_PIN 14
+#define LED3_PIN 15
+#define LED4_PIN 16
 
-#define BUTTON3_PIN 24  // P0.24 - Button SW3
+#define BUTTON3_PIN 24  // P0.24 – SW3
 
-// BLE Services
-BLEDis bledis;
-BLEDfu bledfu;
+// BLE services
+BLEDis  bledis;
+BLEDfu  bledfu;
 BLEUart bleuart;
 
+// Reuse your existing handler; just make sure it drives the LEDs
+// via nrf_gpio and replies via bleuart.println(...)
 void handle_ble_command(char cmd) {
   switch (cmd) {
     case '1':
-      digitalWrite(LED1_PIN, LOW);  Serial.println("LED1 ON"); break;
+      nrf_gpio_pin_clear(LED1_PIN);  bleuart.println("LED1 ON");
+      break;
     case '2':
-      digitalWrite(LED1_PIN, HIGH); Serial.println("LED1 OFF"); break;
+      nrf_gpio_pin_set(LED1_PIN);    bleuart.println("LED1 OFF");
+      break;
     case '3':
-      digitalWrite(LED2_PIN, LOW);  Serial.println("LED2 ON"); break;
+      nrf_gpio_pin_clear(LED2_PIN);  bleuart.println("LED2 ON");
+      break;
     case '4':
-      digitalWrite(LED2_PIN, HIGH); Serial.println("LED2 OFF"); break;
+      nrf_gpio_pin_set(LED2_PIN);    bleuart.println("LED2 OFF");
+      break;
     case '5':
-      digitalWrite(LED3_PIN, LOW);  Serial.println("LED3 ON"); break;
+      nrf_gpio_pin_clear(LED3_PIN);  bleuart.println("LED3 ON");
+      break;
     case '6':
-      digitalWrite(LED3_PIN, HIGH); Serial.println("LED3 OFF"); break;
+      nrf_gpio_pin_set(LED3_PIN);    bleuart.println("LED3 OFF");
+      break;
     case '7':
-      digitalWrite(LED4_PIN, LOW);  Serial.println("LED4 ON"); break;
+      nrf_gpio_pin_clear(LED4_PIN);  bleuart.println("LED4 ON");
+      break;
     case '8':
-      digitalWrite(LED4_PIN, HIGH); Serial.println("LED4 OFF"); break;
+      nrf_gpio_pin_set(LED4_PIN);    bleuart.println("LED4 OFF");
+      break;
     case 'b':
-      bleuart.println(digitalRead(BUTTON3_PIN) == LOW ? "BUTTON3 = PRESSED" : "BUTTON3 = RELEASED");
+      bleuart.println(
+        nrf_gpio_pin_read(BUTTON3_PIN)==0
+        ? "BUTTON3 = PRESSED"
+        : "BUTTON3 = RELEASED"
+      );
       break;
     default:
-      bleuart.println("Unknown command");
+      bleuart.println("Unknown cmd");
       break;
+  }
+}
+
+// This is the correct signature for Bluefruit's UART RX callback:
+void uart_rx_callback(uint16_t conn_handle) {
+  // Pull all available bytes from bleuart into handle_ble_command()
+  while (bleuart.available()) {
+    char cmd = bleuart.read();
+    handle_ble_command(cmd);
   }
 }
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);
-  Serial.println("BLE LED + DFU + BUTTON demo");
+  Serial.println("=== BLE + LED Control Demo ===");
 
-  // Start BLE stack first
+  // Configure LEDs (active LOW) as outputs and switch them OFF
+  nrf_gpio_cfg_output(LED1_PIN); nrf_gpio_pin_set(LED1_PIN);
+  nrf_gpio_cfg_output(LED2_PIN); nrf_gpio_pin_set(LED2_PIN);
+  nrf_gpio_cfg_output(LED3_PIN); nrf_gpio_pin_set(LED3_PIN);
+  nrf_gpio_cfg_output(LED4_PIN); nrf_gpio_pin_set(LED4_PIN);
+
+  // Configure button with pull-up
+  nrf_gpio_cfg_input(BUTTON3_PIN, NRF_GPIO_PIN_PULLUP);
+
+  // Start BLE
   Bluefruit.begin();
-  Bluefruit.autoConnLed(false); // Disable default Bluefruit LED usage on P0.13
-
-  // Reclaim LED1_PIN from Bluefruit and configure all LEDs (active LOW = LOW = ON)
-  pinMode(LED1_PIN, OUTPUT); digitalWrite(LED1_PIN, HIGH);
-  pinMode(LED2_PIN, OUTPUT); digitalWrite(LED2_PIN, HIGH);
-  pinMode(LED3_PIN, OUTPUT); digitalWrite(LED3_PIN, HIGH);
-  pinMode(LED4_PIN, OUTPUT); digitalWrite(LED4_PIN, HIGH);
-
-  // Setup BUTTON3 input with pull-up
-  pinMode(BUTTON3_PIN, INPUT_PULLUP);
-
-  // BLE configuration
   Bluefruit.setName("nRF52840_DK");
-  Bluefruit.setTxPower(4); // Max transmit power
+  Bluefruit.autoConnLed(false);
 
-  // BLE services
+  // Device info
   bledis.setManufacturer("MindTune");
   bledis.setModel("nRF52840 DK");
   bledis.begin();
 
-  bledfu.begin();    // Enable OTA DFU
-  bleuart.begin();   // BLE UART for command reception
+  // DFU
+  bledfu.begin();
 
-  // Start advertising
+  // UART (Nordic UART Service)
+  bleuart.begin();
+  bleuart.setRxCallback(uart_rx_callback);
+
+  // Advertise all services
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
   Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.addService(bledis);
+  Bluefruit.Advertising.addService(bledfu);
+  Bluefruit.Advertising.addService(bleuart);
   Bluefruit.Advertising.start();
 
-  Serial.println("Ready! Connect via nRF Connect and send 1-8 or 'b'");
+  Serial.println("Advertising Device-Info, DFU & UART services");
 }
 
 void loop() {
-  if (bleuart.available()) {
-    char cmd = bleuart.read();
-    handle_ble_command(cmd);
-  }
+  // Everything is handled in uart_rx_callback()
+  delay(100);
 }
